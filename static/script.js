3d57a10dfe2a15b4e75463140362acfd8c9d55e1
Liyana04 <liyanaaris04@gmail.com>
2025-01-14 12:11:32 +0800

    add new navbar

--- static/script.js
+++ static/script.js
@@ -1,3 +1,14 @@
+const menuIcon = document.querySelector('#menu-icon')
+const navbar = document.querySelector('#navbar')
+
+menuIcon.addEventListener('click',()=>{
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+})
+
+
+
+// below this just try and error
 function openCity(evt, tabName) {
   var i, x, tablinks;
   x = document.getElementsByClassName("tab");
