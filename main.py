b297c723f20c3681ef3f85a87be6a6b7e83c6ddf
Liyana04 <liyanaaris04@gmail.com>
2025-01-22 11:01:24 +0800

    change to fastapi

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_type": item.type, "item_name": item.name,"item_price": item.price, "item_id": item_id}
+    return { "item_name": item.name,"item_price": item.price, "item_id": item_id}
