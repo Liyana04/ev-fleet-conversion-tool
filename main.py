b85188b77412558d66b87a35b9bc59af837171dc
Liyana04 <liyanaaris04@gmail.com>
2025-02-04 10:00:37 +0800

    change css

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from typing import Union
 
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
